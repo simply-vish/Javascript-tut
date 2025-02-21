e79c1cb9af11b76c9c55f2bf7a30e996033dd569
simply-vish <130661664+simply-vish@users.noreply.github.com>
2025-02-21 14:42:56 +0000

    Dates

---
+++ basics/Dates.js
@@ -0,0 +1,38 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toUTCString());
+// console.log(typeof myDate);
+
+//let myCreatedDate = new Date(2023,0,23)       //0 -> jan
+//let myCreatedDate = new Date(2023,0,23,5,3)
+//let myCreatedDate = new Date("2025-02-14")
+let myCreatedDate = new Date("01-14-2023")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+
+// ${newDate.getDay()} and the time
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+    timeZone: ''
+})
+
+
