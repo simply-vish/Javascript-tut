1102fff0c0b9f7cf5aec296eb2671a0683514042
simply-vish <130661664+simply-vish@users.noreply.github.com>
2025-03-21 15:18:17 +0000

    High Order Array Loops

---
+++ 05_iterations/threee.js
@@ -0,0 +1,35 @@
+// for of
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1,2,3,4,5]
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "Hello worl!"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`); 
+}
+
+// Maps -> unique values
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+
+// console.log(map);
+
+for (const [key,value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1 : 'NFS',
+    game2 : 'Spiderman'
+}
+
+// for (const [key,value] of object) {
+//     console.log(key, ':-', value); 
+// }
